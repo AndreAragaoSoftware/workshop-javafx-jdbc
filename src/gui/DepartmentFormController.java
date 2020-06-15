3a4391e555ffafd126523d7fe817eb5994e426f3
André Aragão <64162795+AndreAragaoSoftware@users.noreply.github.com>
2020-06-15 17:39:26 -0300

    Validation exception

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -3,7 +3,9 @@ package gui;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.Set;
 
 import db.DbException;
 import gui.listeners.DataChangeListener;
@@ -18,6 +20,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Department;
+import model.exceptions.ValidationException;
 import model.services.DepartmentService;
 
 public class DepartmentFormController implements Initializable {
@@ -72,6 +75,8 @@ public class DepartmentFormController implements Initializable {
 			
 		} catch (DbException e) {
 			Alerts.showAlert("Error saving object", null, e.getMessage(), AlertType.ERROR);
+		}catch(ValidationException e) {
+			setErrorMessages(e.getErrors());
 		}
 
 	}
@@ -86,11 +91,25 @@ public class DepartmentFormController implements Initializable {
 	private Department getFormData() {
 		// instanciando o obj no Department
 		Department obj = new Department();
+		
+		//instanciando a exception
+		ValidationException exception = new ValidationException("Validation error");
 
 		// pegando o texto da caixa txtId
 		// foi utilizado o metodo tryParse para tranformar em int
 		obj.setId(Utils.tryParseToInt(txtId.getText()));
+		
+		// o .trim() serve para eliminar os espaços em branco
+		if(txtName.getText() == null || txtName.getText().trim().equals("")) {
+			//foi add um erro caso o campo estiver vazio
+			exception.addError("name", "Fiel can't be empty");
+		}
 		obj.setName(txtName.getText());
+		
+		//testando pra ve se existe pelomenos um erro 
+		if(exception.getErrors().size() > 0) {
+			throw exception;
+		}
 
 		return obj;
 	}
@@ -121,6 +140,15 @@ public class DepartmentFormController implements Initializable {
 		// valueOf foi pra converter o id para String
 		txtId.setText(String.valueOf(entity.getId()));
 		txtName.setText(entity.getName());
+	}
+	
+	//dando um set na labelErrorName caso exista um erro
+	private void setErrorMessages(Map<String, String> errors) {
+		Set<String> fields = errors.keySet();
+		
+		if(fields.contains("name")) {
+			labelErrorName.setText(errors.get("name"));
+		}
 	}
 
 }
